783209a2cc945f996c18fc591b93b44708c9862f
IndumathiN <indu.dharshu@gmail.com>
2025-12-19 17:18:05 -0500

    changed config url to render

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 //export const BASE_URL = "https://primary-production-afa5.up.railway.app/webhook-test";
 //export const BASE_URL = "https://mite-kind-neatly.ngrok-free.app/webhook";
-export const BASE_URL = "https://n8n.numpyninja.com/webhook";
+//export const BASE_URL = "https://n8n.numpyninja.com/webhook";
+export const BASE_URL = "https://n8n-haq.onrender.com/webhook";
